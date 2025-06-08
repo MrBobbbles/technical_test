de8a2c96c07f417f7f0fa5480c90f6636e2f474c
TJ Hornsey <tj.hornsey@gmail.com>
2025-06-08 16:38:29 +0100

    adding 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "errors/not_found"
+  get "errors/internal_server_error"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -16,4 +18,9 @@ Rails.application.routes.draw do
   get "galleries/:id/slideshow", to: "galleries#slideshow", as: "gallery_slideshow"
 
   root "galleries#index"
+
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+  match '*unmatched', to: 'errors#not_found', via: :all
+
 end
