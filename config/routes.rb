2560265b62f245d1b3ae8c7412b42e61677fd1e8
TJ Hornsey <tj.hornsey@gmail.com>
2025-06-08 16:50:49 +0100

    removing unmatched route

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,6 @@ Rails.application.routes.draw do
 
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
-  match '*unmatched', to: 'errors#not_found', via: :all
+
 
 end
